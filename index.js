bb4424b4944ff5a00c74287761b063e48c96b947
Dan de Havilland <dan@dandehavilland.com>
2016-01-13 16:45:00 +0100

    Fixing deps for app

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ module.exports = {
     app.options['ember-cli-chosen'] = options;
 
     // Import the correct JS for chosen
-    app.import(app.bowerDirectory + '/chosen/chosen.' + options.chosenJSType + '.js');
+    app.import(app.bowerDirectory + '/chosen/public/chosen.' + options.chosenJSType + '.js');
 
     // Import Chosen CSS (done by default)
-    if(options.importChosenCSS) { app.import(app.bowerDirectory + '/chosen/chosen.css'); }
+    if(options.importChosenCSS) { app.import(app.bowerDirectory + '/chosen/public/chosen.css'); }
   },
   treeForPublic: function(treeName) {
     var tree;
 
     // Only include the Chosen sprites if we're including Chosen CSS in the build
     if(this.app.options['ember-cli-chosen'].importChosenCSS) {
-      tree = pickFiles(this.app.bowerDirectory + '/chosen', {
+      tree = pickFiles(this.app.bowerDirectory + '/chosen/public', {
         srcDir: '/',
         files: ['*.png'],
         destDir: '/assets'
